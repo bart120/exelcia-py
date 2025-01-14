96dd5c532d9d45e35de1a2766fd76cbfde82dd48
Vincent Leclerc <vleclerc@inow.fr>
2025-01-14 17:36:52 -0400

    delete

--- api/routers/cars.py
+++ api/routers/cars.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Response
 from schema import Car, CarCreate
 from typing import List
 from database import get_db
@@ -14,7 +14,10 @@ def get_cars(db: Session = Depends(get_db)): #récupère le yield de get_db dans
 
 @router.get("/{car_id}", response_model=Car)
 def get_car_by_id(car_id:int, db: Session = Depends(get_db)):
-    db_car = db.query(CarModel).filter(CarModel.id == car_id).first()
+    #db_car = db.query(CarModel).filter(CarModel.id == car_id).first()
+    db_car = db.query(CarModel).filter(CarModel.id == car_id).one_or_none()
+    if(db_car is None):
+        raise HTTPException(status_code=404, detail="Car not found")
     return db_car
 
 @router.post("/", response_model=Car)
@@ -24,3 +27,23 @@ def create_car(car: CarCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_car)
     return db_car
+
+@router.put("/{car_id}", response_model=Car)
+def update_car(car: CarCreate, car_id:int, db: Session = Depends(get_db)):
+    db_car = db.query(CarModel).filter(CarModel.id == car_id).one_or_none()
+    if(db_car is None):
+        raise HTTPException(status_code=404, detail="Car not found")
+    update_data = car.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_car, key, value)
+    db.commit()
+    return db_car
+
+@router.delete("/{car_id}")
+def delete_car(car_id:int, db: Session = Depends(get_db)):
+    db_car = db.query(CarModel).filter(CarModel.id == car_id).one_or_none()
+    if(db_car is None):
+        raise HTTPException(status_code=404, detail="Car not found")
+    db.delete(db_car)
+    db.commit()
+    return Response(status=204)
